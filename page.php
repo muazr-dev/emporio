ad39c2d320dc192a487ecdc24bf23b8c61886b7b
Muaz Rehan <157925503+muazr-dev@users.noreply.github.com>
2024-03-24 10:43:31 +0500

    title and site link

---
+++ page.php
@@ -0,0 +1,12 @@
+<?php
+get_header();?>
+<header class="theme-bg-light text-center gradient ">
+<h1 class="heading"><?php the_title() ?></h1>
+
+
+<h2><a href="<?php echo site_url()?>"></a>Emporio</h2>
+
+
+</header>
+
+<?php get_footer();   ?>
