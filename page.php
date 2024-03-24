ffb6bb3a35cdcaa33bd18270f8072f8f8c4b7921
Muaz Rehan <157925503+muazr-dev@users.noreply.github.com>
2024-03-24 10:55:45 +0500

    get content or the_content

--- page.php
+++ page.php
@@ -1,12 +1,16 @@
 <?php
-get_header();?>
-<header class="theme-bg-light text-center gradient ">
-<h1 class="heading"><?php the_title() ?></h1>
+get_header();
+the_post();
 
-
-<h2><a href="<?php echo site_url()?>"></a>Emporio</h2>
-
-
+?>
+<header class=" text-center gradient ">
+    <h1 class=""><?php the_title(); ?></h1>
 </header>
+
+<h2><a href="<?php echo site_url(); ?>"></a>Emporio</h2>
+
+<!-- <div class="main-content"><?php echo get_the_content(); ?></div> -->
+<div class="main-content"><?php the_content(); ?></div>
+
 
 <?php get_footer();   ?>
